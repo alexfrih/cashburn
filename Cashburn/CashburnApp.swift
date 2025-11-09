a18bc692c262b8ce9f82a883d89f438cfc07df6b
Alexandre <alexandre.frih@solarbeam.studio>
2025-11-09 03:25:01 +0100

    Initial Commit

---
+++ Cashburn/CashburnApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashburnApp.swift
+//  Cashburn
+//
+//  Created by Alexandre on 09/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CashburnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CashburnTests/CashburnTests.swift
@@ -0,0 +1,17 @@
+//
+//  CashburnTests.swift
+//  CashburnTests
+//
+//  Created by Alexandre on 09/11/2025.
+//
+
+import Testing
+@testable import Cashburn
+
+struct CashburnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
